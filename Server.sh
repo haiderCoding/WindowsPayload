019d6365072cab05ce03d5f2905627245bf939e3
haiderCoding <114228135+haiderCoding@users.noreply.github.com>
2022-09-23 16:53:25 +0500

    Update Server.sh

--- Server.sh
+++ Server.sh
@@ -30,22 +30,6 @@ echo -e $'\e[1;91m\e[0m\e[1;33m\e[0m\e[1;90m\e[0m\e[1;92m !!    Installation Suc
 echo -e $'\e[1;91m\e[0m\e[1;33m\e[0m\e[1;96m\e[0m\e[1;92m  ---------------------------------   \e[1;91m\e[0m'
 sleep 4
 clear
-echo ""
-                           echo -e $'\e[1;33m\e[0m\e[1;77m \e[0m\e[1;33m\e[0m\e[1;36m       ---------------------------        \e[0m'
-                           echo -e $'\e[1;33m\e[0m\e[1;77m \e[0m\e[1;33m\e[0m\e[1;91m     !   PHP SERVER NOW STARTING   !      \e[0m'
-                           echo -e $'\e[1;33m\e[0m\e[1;77m \e[0m\e[1;33m\e[0m\e[1;36m       ---------------------------        \e[0m'
-                           php -S 127.0.0.1:4444 > /dev/null 2>&1 &
-                           sleep 3
-                           echo ""
-                           echo -e $'\e[1;33m\e[0m\e[1;77m \e[0m\e[1;33m\e[0m\e[1;36m       ---------------------------        \e[0m'
-                           echo -e $'\e[1;33m\e[0m\e[1;77m \e[0m\e[1;33m\e[0m\e[1;91m     !  NGROK SERVER NOW STARTING  !     \e[0m'
-                           echo -e $'\e[1;33m\e[0m\e[1;77m \e[0m\e[1;33m\e[0m\e[1;36m       ---------------------------        \e[0m'
-                           ./ngrok http 4444 > /dev/null 2>&1 &
-                           sleep 25
-                           clear
-                           if [[ -e log.txt ]]; then
-                           > log.txt
-                           fi 
-                           echo ""
-                           echo ""
-                           tail -f log.txt 
+chmod x+a script.sh
+sleep 1
+bash script.sh
